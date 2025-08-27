c1627773d76864afe92901089b413e5d4491b7d7
anchoriumlink <anchoriumlink@gmail.com>
2025-08-27 16:00:56 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AnchoriumLink",
-            url: "https://github.com/anchoriumlink/AnchoriumLink-SPM/releases/download/1.0.0/AnchoriumLink.xcframework.zip",
+            url: "https://github.com/anchoriumlink/AnchoriumLink-SPM/releases/download/1.0.0/anchoriumlinkframework.xcframework.zip",
             checksum: "d78374192066f3766be2518e64841b31ac617804e37f203bc0642d6c22b3bfd4"
         )
     ]
