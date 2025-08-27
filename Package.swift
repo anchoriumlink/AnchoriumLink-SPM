5ee747583cd17ae47e6f5870d1ee6ef489b373f2
anchoriumlink <anchoriumlink@gmail.com>
2025-08-27 15:54:48 +0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "AnchoriumLink",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "AnchoriumLink",
+            targets: ["AnchoriumLink"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AnchoriumLink",
+            url: "https://github.com/anchoriumlink/AnchoriumLink-SPM/releases/download/1.0.0/AnchoriumLink.xcframework.zip",
+            checksum: "d78374192066f3766be2518e64841b31ac617804e37f203bc0642d6c22b3bfd4"
+        )
+    ]
+)
